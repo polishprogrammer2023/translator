e0ae05204e91c01787201162a0d489f19cf73387
U-lenovo\Piotruś <piotrszapanski123@gmail.com>
2026-09-13 13:51:42 +0200

    added credits to mymemory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 
+print("This translator was made using the MyMemory API")
 word = input("Input sentence to translate> ")
 lang_from = input("Input input language(ISO 639-1)> ")
 lang_to = input("Input output language(ISO 639-1)> ")
