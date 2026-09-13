9a47c1e34cfb8b2b859406c61c87619888a06dd2
U-lenovo\Piotruś <piotrszapanski123@gmail.com>
2026-09-13 13:43:50 +0200

    initial translator - pretty barebones

---
+++ main.py
@@ -0,0 +1,8 @@
+import requests
+
+word = input("Input sentence to translate> ")
+lang_from = input("Input input language(ISO 639-1)> ")
+lang_to = input("Input output language(ISO 639-1)> ")
+translation = requests.get(f"https://api.mymemory.translated.net/get?q={word}&langpair={lang_from}|{lang_to}").json()
+
+print(translation["responseData"]["translatedText"])
